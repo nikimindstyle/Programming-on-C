86ecddb2ca16f6e92cf3cb84f10a9050bf4b8e1d
Nikimindstyle <nikita.ananko.15062007@gmail.com>
2026-03-20 09:34:26 +1000

    retry

--- Labs/Lab 4/triangle.h
+++
@@ -1,8 +0,0 @@
-#ifndef TRIANGLE_H
-#define TRIANGLE_H
-
-double get_perimeter(double a, double b, double c);
-double get_area(double a, double b, double c);
-int is_valid_triangle(double a, double b, double c);
-
-#endif
