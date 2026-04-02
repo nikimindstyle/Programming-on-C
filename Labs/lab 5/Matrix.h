6f18a5b6e1251fd60d658f6738c93a6661741817
Nikimindstyle <nikita.ananko.15062007@gmail.com>
2026-04-02 21:00:52 +1000

    commit

--- Labs/lab 5/Matrix.h
+++
@@ -1,8 +0,0 @@
-#ifndef MATRIX_H
-#define MATRIX_H
-double** matrix_plus(double** A, double** B, int n);
-double** matrix_minus(double** A, double** B, int n);
-double** matrix_multi(double** A, double** B, int n);
-double** calc_matrix(double** A, double** B, int n, char op);
-
-#endif
